f186910812b68377e9f4480d1e366f9a22347f84
Binh <binh.phan@takeaway.com>
2020-07-20 20:08:58 +0200

    widget changes

--- app/src/main/java/com/riceplant/capstoneproject/data/GameWidget.java
+++ app/src/main/java/com/riceplant/capstoneproject/data/GameWidget.java
@@ -15,10 +15,8 @@ public class GameWidget extends AppWidgetProvider {
     static void updateAppWidget(Context context, AppWidgetManager appWidgetManager,
                                 int appWidgetId) {
 
-        CharSequence widgetText = context.getString(R.string.appwidget_text);
         // Construct the RemoteViews object
         RemoteViews views = new RemoteViews(context.getPackageName(), R.layout.game_widget);
-        views.setTextViewText(R.id.appwidget_text, widgetText);
 
         // Instruct the widget manager to update the widget
         appWidgetManager.updateAppWidget(appWidgetId, views);
