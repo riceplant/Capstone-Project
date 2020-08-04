0c6bf1bb331b43eacf0c787a8210fa2ef821d228
Binh <binh.phan@takeaway.com>
2020-08-04 17:28:25 +0200

    added new class for widget

--- app/src/main/java/com/riceplant/capstoneproject/data/GameWidget.java
+++ app/src/main/java/com/riceplant/capstoneproject/data/GameWidget.java
@@ -22,9 +22,6 @@ import java.util.List;
  */
 public class GameWidget extends AppWidgetProvider {
 
-    private static ArrayList<MyGame> mGamesLibrary = new ArrayList<>();
-    private static LiveData<List<MyGame>> mGame;
-
     static void updateAppWidget(Context context, AppWidgetManager appWidgetManager,
                                 int appWidgetId) {
 
@@ -52,72 +49,6 @@ public class GameWidget extends AppWidgetProvider {
     @Override
     public void onDisabled(Context context) {
         // Enter relevant functionality for when the last widget is disabled
-    }
-
-    public class GameWidgetRemoteService extends RemoteViewsService {
-
-        @Override
-        public RemoteViewsFactory onGetViewFactory(Intent intent) {
-            return new GameWidgetRemoteViewsFactory(this.getApplicationContext(), intent);
-        }
-
-        class GameWidgetRemoteViewsFactory implements RemoteViewsService.RemoteViewsFactory {
-
-            private GameDao gameDao;
-            private LiveData<List<MyGame>> mGames;
-
-            GameWidgetRemoteViewsFactory(Context context, Intent intent) {
-                GameRoomDatabase db = GameRoomDatabase.getInstance(context);
-                gameDao = db.gameDao();
-            }
-
-            @Override
-            public void onCreate() {
-
-            }
-
-            @Override
-            public void onDataSetChanged() {
-                mGames = gameDao.loadAllGames();
-                mGame = mGames;
-            }
-
-            @Override
-            public void onDestroy() {
-
-            }
-
-            @Override
-            public int getCount() {
-                return 0;
-            }
-
-            @Override
-            public RemoteViews getViewAt(int position) {
-                return null;
-            }
-
-            @Override
-            public RemoteViews getLoadingView() {
-                return null;
-            }
-
-            @Override
-            public int getViewTypeCount() {
-                return 0;
-            }
-
-            @Override
-            public long getItemId(int position) {
-                return 0;
-            }
-
-            @Override
-            public boolean hasStableIds() {
-                return false;
-            }
-        }
-
     }
 }
 
